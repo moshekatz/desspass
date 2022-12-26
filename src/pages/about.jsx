0fc017b9914d234b976da2a4e0832ce467071a14
Moshe Katz <moshe.katz@hey.com>
2022-12-26 19:29:21 +0200

    download cv

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -96,7 +96,7 @@ export default function About() {
               </SocialLink>
             </ul>
             <Button href="#" variant="secondary" className="group mt-6 w-full">
-              <a href="/files/linoy-avraham-resume.pdf" target="_blank" rel="noopener noreferrer">Download CV</a>
+              <a download href="/files/linoy-avraham-resume.pdf">Download CV</a>
               <ArrowDownIcon className="h-4 w-4 stroke-zinc-400 transition group-active:stroke-zinc-600 dark:group-hover:stroke-zinc-50 dark:group-active:stroke-zinc-50" />
       </Button>
           </div>
