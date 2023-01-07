4a2393ab9c173c6ec58e57b059da7a4f74bc2a7b
Moshe Katz <moshe.katz@hey.com>
2023-01-07 13:53:42 +0200

    yalla

--- src/pages/projects/index.jsx
+++ src/pages/projects/index.jsx
@@ -34,10 +34,12 @@ export default function Projects() {
         >
           {projects.map((project) => (
             <Card as="li" key={project.name}>
-              <Image
-                  src={project.logo}
-                  alt={`${project.name} cover photo`}
-                />
+              <div className="relative z-10 flex shadow-md shadow-zinc-800/5 ring-1 ring-zinc-900/5 dark:border dark:border-zinc-700/50 dark:bg-zinc-800 dark:ring-0">
+                <Image
+                    src={project.logo}
+                    alt={`${project.name} cover photo`}
+                  />
+              </div>
               <h2 className="mt-6 text-base font-semibold text-zinc-800 dark:text-zinc-100">
                 <Card.Link href={project.link.href}>{project.name}</Card.Link>
               </h2>
