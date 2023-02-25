2a49b48a1cc3039165eb62bce8380595e1ee66f8
Moshe Katz <moshe.katz@hey.com>
2023-02-25 17:00:00 +0200

    wip

--- src/pages/articles/index.jsx
+++ src/pages/articles/index.jsx
@@ -34,6 +34,9 @@ function Article({ article }) {
   )
 }
 
+const description = 'Writing on design, creation and practical everyday tips';
+const intro = 'A curated collection of inspirational boards, materials, color palettes and other stuff that I wanted to bookmark and share.';
+
 export default function ArticlesIndex({ articles }) {
   return (
     <>
@@ -41,12 +44,12 @@ export default function ArticlesIndex({ articles }) {
         <title>Articles - Linoy Avraham</title>
         <meta
           name="description"
-          content="All of my long-form thoughts on programming, leadership, product design, and more, collected in chronological order."
+          content={description}
         />
       </Head>
       <SimpleLayout
-        title="Writing on software design, company building, and the aerospace industry."
-        intro="All of my long-form thoughts on programming, leadership, product design, and more, collected in chronological order."
+        title={description}
+        intro={intro}
       >
         <div className="md:border-l md:border-zinc-100 md:pl-6 md:dark:border-zinc-700/40">
           <div className="flex max-w-3xl flex-col space-y-16">
