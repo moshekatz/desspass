24329a004b53130ef83a0f7b3b697a06b4a8d293
Moshe Katz <moshe.katz@hey.com>
2023-03-04 13:37:32 +0200

    wip

--- src/pages/articles/index.jsx
+++ src/pages/articles/index.jsx
@@ -35,7 +35,7 @@ function Article({ article }) {
 }
 
 const description = 'Writing on design, creation and practical everyday tips';
-const intro = 'A curated collection of inspirational boards, materials, color palettes and other stuff that I wanted to bookmark and share.';
+const intro = 'A curated collection of inspirational boards, materials, color palettes and other stuff that I wanted to bookmark and share. I highly recommend you to press play.';
 
 export default function ArticlesIndex({ articles }) {
   return (
@@ -51,6 +51,7 @@ export default function ArticlesIndex({ articles }) {
         title={description}
         intro={intro}
       >
+        <iframe style={{ borderRadius: 12 }} className="max-w-3xl -mt-8 mb-16" src="https://open.spotify.com/embed/playlist/0JoDPg3BRJy8DWOeMCvwwo?utm_source=generator&theme=0" width="100%" height="152" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
         <div className="md:border-l md:border-zinc-100 md:pl-6 md:dark:border-zinc-700/40">
           <div className="flex max-w-3xl flex-col space-y-16">
             {articles.map((article) => (
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -374,6 +374,7 @@ export default function Home({ articles }) {
             {/* <Newsletter /> */}
             <Resume />
             <Education />
+            <iframe style={{ borderRadius: 12 }} src="https://open.spotify.com/embed/playlist/0JoDPg3BRJy8DWOeMCvwwo?utm_source=generator&theme=0" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
           </div>
         </div>
       </Container>
